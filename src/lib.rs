f8a5c15a385e1658947ba74d865b14495e3b37d3
Sosthène Guédon <sosthene@nitrokey.com>
2023-06-06 16:14:55 +0200

    Change license to Apache+MIT
    This is more permissive for use in trussed

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 // Copyright (C) 2022 Nitrokey GmbH
-// SPDX-License-Identifier: LGPL-3.0-only
+// SPDX-License-Identifier: Apache-2.0 or MIT
 
 #![cfg_attr(not(test), no_std)]
 #![doc = include_str!("../README.md")]
